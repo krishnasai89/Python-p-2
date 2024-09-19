3ae6472589b9f740b05196db4b248cfcf8a0f903
vellampallikrishnasaigifhub <krishnasaivellampalli@gmail.com>
2024-09-19 14:55:09 +0530

    update

---
+++ Countdown timer/main.py
@@ -0,0 +1,10 @@
+from time import *
+
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(my_time,0,-1):
+    seconds = x % 60
+    print(f"00:00:{seconds}")
+    sleep(1)
+
+print("TIME'S UP")
